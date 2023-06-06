fe9035124a88ff6eef179e48a45b385e6b762172
TravTheBav <tzito93@gmail.com>
2023-06-05 20:02:18 -0400

    Style flash messages and add dismiss button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/landing'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "static_pages#landing"
+  root to: 'static_pages#landing'
+  get 'landing', to: 'static_pages#landing', as: :landing
+  get 'about', to: 'static_pages#about', as: :about
 end
