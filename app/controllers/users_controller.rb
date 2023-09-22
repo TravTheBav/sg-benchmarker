b53c47eb5ee60f3fcc8354d8bcfd80ed024c721e
TravTheBav <tzito93@gmail.com>
2023-09-22 17:40:51 -0400

    bootstrap modal with layout for avatar selection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
+require 'avatar'
+
 class UsersController < ApplicationController
     load_and_authorize_resource :user
 
     def show
+      @avatars = Avatar.all
     end
 
     def account_info
