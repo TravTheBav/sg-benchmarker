778a4a07ad84956191cecdcc3c72051ee3a02525
TravTheBav <tzito93@gmail.com>
2024-06-05 13:29:02 -0400

    initial setup for Render deployment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,9 +36,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -67,6 +64,22 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # lines added for devise password reset
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: 'tzito93@gmail.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port:    587,
+    domain:  'sg_benchmarker.com',
+    user_name: Rails.application.credentials.gmail[:email],
+    password:  Rails.application.credentials.gmail[:password],
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
